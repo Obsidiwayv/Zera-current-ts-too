0409399980016b1f461a4ac9b59908132d163d9f
Obsidiwayv <pizza5803@gmail.com>
2023-10-19 23:54:30 -0400

    delete /build

--- binaries/build/ZeraClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const dysnomia_1 = require("@projectdysnomia/dysnomia");
-module.exports = class ZeraClient extends dysnomia_1.Client {
-    constructor() {
-        super("");
-    }
-};
